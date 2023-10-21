43f02b4911f050a215a75c74fdad575429abe344
Hoang Nguyen <nmhoang2909@gmail.com>
2023-10-21 16:35:47 +0700

    update treesitter

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,29 +1,41 @@
-local M = {
-}
-function M.config()
-  local treesitter = require "nvim-treesitter"
-  local configs = require "nvim-treesitter.configs"
-
-  configs.setup {
-    ensure_installed = "all", -- one of "all" or a list of languages
-    ignore_install = { "" },                                                       -- List of parsers to ignore installing
-    sync_install = false,                                                          -- install languages synchronously (only applied to `ensure_installed`)
-    auto_install = true,
-
-    highlight = {
-      enable = true,       -- false will disable the whole extension
-      disable = { "css" }, -- list of language that will be disabled
-    },
-    autopairs = {
-      enable = true,
-    },
-    indent = { enable = true, disable = { "python", "css", "yaml" } },
-
-    context_commentstring = {
-      enable = true,
-      enable_autocmd = false,
-    },
-  }
+local status_ok, configs = pcall(require, 'nvim-treesitter.configs')
+if not status_ok then
+  return
 end
 
-return M
+configs.setup {
+  ensure_installed = 'all', -- 'all', or a list of languages
+  sync_install = false,     -- install languages synchronously (only applied to `ensure_installed`)
+  ignore_install = { '' },  -- list of parsers to ignore installing
+  autopairs = {
+    enable = true,
+  },
+  highlight = {
+    enable = true,    -- false will disable the whole extension
+    disable = { '' }, -- list of language that will be disabled
+    additional_vim_regex_highlighting = true,
+  },
+  indent = { enable = true, disable = { 'yaml' } },
+  context_commentstring = {
+    enable = true,
+    enable_autocmd = false,
+  },
+  playground = {
+    enable = true,
+    disable = {},
+    updatetime = 25,         -- debounced time for highlighting nodes in the playground from source code
+    persist_queries = false, -- whether the query persists across vim sessions
+    keybindings = {
+      toggle_query_editor = 'o',
+      toggle_hl_groups = 'i',
+      toggle_injected_languages = 't',
+      toggle_anonymous_nodes = 'a',
+      toggle_language_display = 'I',
+      focus_language = 'f',
+      unfocus_language = 'F',
+      update = 'R',
+      goto_node = '<CR>',
+      show_help = '?',
+    },
+  },
+}
